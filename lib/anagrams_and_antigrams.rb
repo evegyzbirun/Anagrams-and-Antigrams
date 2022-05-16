02260fffbd239f259d519019982c8ec6a532a9eb
Evgeny Zbirun <zbirunzbirun@gmail.com>
2022-05-16 00:00:55 -0700

    fixed functions and added test when sentences are antigrams.

--- lib/anagrams_and_antigrams.rb
+++ lib/anagrams_and_antigrams.rb
@@ -1,68 +1,23 @@
+require('pry')
+
 class AaA 
+  attr_reader :wordOne, :wordTwo
   def initialize(wordOne, wordTwo)
-    @wordOne = wordOne
-    @wordTwo = wordTwo
-    
+    @wordOne = wordOne.downcase.delete(' ')
+    @wordTwo = wordTwo.downcase.delete(' ')
+    @wordOne.gsub(/[A-Za-z]/,'')
+    @wordTwo.gsub(/[A-Za-z]/,'')
   end
-  #1
-  def anagrams 
-    if (sort(@wordOne) == sort(@wordTwo))
+
+  def anagram()
+    if !(@wordOne =~ /[aeiouy]/) && !(@wordTwo =~ /[aeiouy]/)
+      "These words are not words."
+    elsif (@wordOne.chars.sort == @wordTwo.chars.sort)
       "These words are anagrams."
-    else 
-      "These words are not anagrams."
+    else (@wordOne.chars.sort != @wordTwo.chars.sort)
+      "These words are antigrams."
     end
-  end
-  def sort (stringMain)
-    stringMain.chars.sort.join
-  end
- #2
-  def bothWords
-    if (sort(@wordTwo.downcase())) == (sort(@wordOne.downcase()))
-      true
-    else
-      false
-    end
-  end
-  #3
-  def actual_words 
-    array1 = @wordOne.split('')
-    array2 = @wordTwo.split('')
-    
-    ## .none if array1 or array2 don't have aeiouy
-  
-    if (array1.none? {|i| i.match("aieouy")}) || (array2.none? {|i| i.match("aieouy")}) 
-    "You need to input actual words!"
-    else
-    false
-    end
-  end
-
-  #4
-  def antigrams
-    array1 = @wordOne.split()
-    array2 = @wordTwo.split()
-    if array1.any? {|array1|array2.include?(array1)}
-    "These words have some letter matches."
-    else
-      "These words have no letter matches and are antigrams."
-    end
-  end
-  #5
-  def sentences_match 
-    array1 = @wordOne.split(" ")
-    array2 = @wordTwo.split(" ")
-    array11 = array1.each(i).split('')
-    array22 = array2.each(i).split('')
-    #string of sentences split on words and chech if they are real words
-    #if all real words unite in each input in one word to check for anagram
-    if array11.any? {|array11|array22.include?(array11)}
-    "These words have some letter matches."
-    else
-      "These words have no letter matches and are antigrams."
-    end
-
-    
-  
-
   end
 end
+
+ 
--- spec/aaa_spec.rb
+++ spec/aaa_spec.rb
@@ -1,34 +1,33 @@
-require('pry')
+
 require('rspec')
 require('anagrams_and_antigrams')
 
+
 describe("#AaA") do
+  describe('#anagram') do
   it("Check if two words are anagrams") do 
-    check = AaA.new('time','tmei')
-    expect(check.anagrams).to(eq("These words are anagrams."))
+    check = AaA.new("time","imte")
+    expect(check.anagram).to(eq("These words are anagrams."))
   end
   it("Check if two words are not anagrams") do 
-    check = AaA.new('test','ttase')
-    expect(check.anagrams).to(eq("These words are not anagrams."))
+    check = AaA.new("red","good")
+    expect(check.anagram).to(eq("These words are antigrams."))
   end
   it("words might have different cases but should still be anagrams") do 
     check = AaA.new('tEA','EaT')
-    expect(check.anagrams).to(eq(true))
+    expect(check.anagram).to(eq("These words are anagrams."))
   end
   it("check if the inputs are words, so if they have vowel (a, i, e, o, u) or y") do 
     check = AaA.new('pkmgy','mgpky')
-    expect(check.actual_words).to(eq("You need to input actual words!"))
+    expect(check.anagram).to(eq("These words are anagrams."))
   end
-  it("If phrases aren't anagrams, the method should check whether they are actuallyantigrams") do 
-    check = AaA.new('hizr','bye')
-    expect(check.antigrams).to(eq("These words have no letter matches and are antigrams."))
-  end
-  it("Account for two sentences being compared as anagrams or antigrams") do 
+  it("What are the words in the two sentences anagrams or antigrams?") do 
     check = AaA.new('The Morse Code','Here come dots')
-    expect(check.sentences_match).to(eq("the two sentences are anagrams." ))
+    expect(check.anagram).to(eq("These words are anagrams." ))
   end
-  #it("Account for two sentences being compared as anagrams or antigrams") do 
-    #check = AaA.new('The Morse Code','Here come dots!')
-    #expect(check.sentences_match).to(eq("the two sentences are antigrams." ))
-  #end
+  it("What are the words in the two sentences anagrams or antigrams?") do 
+    check = AaA.new('Hi','bye bye')
+    expect(check.anagram).to(eq("These words are antigrams." ))
+  end
+end
 end
