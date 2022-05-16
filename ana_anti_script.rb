1f5304a752cc5664f02c5a5bf67f6c1cd8077ac4
Evgeny Zbirun <zbirunzbirun@gmail.com>
2022-05-16 00:35:21 -0700

    fixed script file.

---
+++ ana_anti_script.rb
@@ -0,0 +1,13 @@
+#!/usr/bin/ruby
+require('./lib/anagrams_and_antigrams')
+efefefe
+puts 'Welcome to the Anagrams and Antigrams'
+puts 'Please enter a first word'
+wordOne = gets.chomp
+puts 'Please enter a second word'
+wordTwo = gets.chomp
+puts "The first word  you entered is #{wordOne.string}"
+puts "The second word you entered is #{wordTwo.string}"
+final = AaA.new(wordOne, wordTwo)
+result = final.anagram()
+
